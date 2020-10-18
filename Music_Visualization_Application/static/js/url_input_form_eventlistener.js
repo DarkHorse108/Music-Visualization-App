92a4771972cedab1c4140a2d8e63fb783766a4ba
John Sy <13071774+DarkHorse108@users.noreply.github.com>
2020-10-17 21:02:32 -1000

    Added module that collects user input from the URL input form upon submission.

--- Music_Visualization_Application/static/js/url_input_form_eventlistener.js
+++ Music_Visualization_Application/static/js/url_input_form_eventlistener.js
@@ -2,13 +2,13 @@
 let user_url = '';
 
 // We capture the form HTML element associated with the submission form for the SoundCloud URL by the user
-const form_element = document.querySelector("#user_form_element");
+const form_element = document.getElementById("user_form_element");
 
 // We add an eventlistener when the form has been submitted (i.e. when the submit button is pressed). We prevent refreshing of the page
 // and then take the string value the user entered in the form and store it in the variable user_url. We then clear the form of any text and print the captured value.
 form_element.addEventListener("submit", function(event){
 	event.preventDefault();
-	user_url = document.querySelector("#user_input_url").value;
+	user_url = document.getElementById("user_input_url").value;
 	form_element.reset();
 	console.log(user_url);
 });
