57b9b4a0b208acc9897fddd5c580ef083209672d
John Sy <13071774+DarkHorse108@users.noreply.github.com>
2020-10-23 04:57:26 -1000

    Added the soundcloud_request_module created by Samnang Penh
    Takes a track url and a SoundCloud client id in order to obtain the direct url for the song's stream, as well as other information such as artist name, song title, song length, song artwork, as provided by the SoundCloud API.

---
+++ Music_Visualization_Application/static/js/soundcloud_request_module.js
@@ -0,0 +1,42 @@
+export async function requestTrack(url, id) {
+  // Check url length and if it includes a valid prefix
+  if (url.length < 24 || !url.includes("https://soundcloud.com/")) {
+    throw new Error("Invalid link!");
+  }
+
+  // Request JSON from SoundCloud server and await response
+  const getTrackData = await fetch(
+    `https://api.soundcloud.com/resolve?url=${url}&client_id=${id}`
+  );
+
+  // Parse response as JSON and store select keys into trackData object
+  if (getTrackData.status === 200) {
+    const data = await getTrackData.json();
+    return {
+      streamSource: `${data.stream_url}?client_id=${id}`,
+      artworkSource: data.artwork_url.replace("large", `t250x250`),
+      artist: `${data.user["username"]}`,
+      title: `${data.title}`,
+      duration: `${convertToMinSec(Number(data.duration))}`,
+    };
+  } else {
+    throw new Error("Cannot acquire track info");
+  }
+}
+
+// Convert from milliseconds to MM:SS format
+function convertToMinSec(duration) {
+  // Convert total track duration from milliseconds to minutes
+  const totalMinutes = duration / 60000;
+
+  // Calculate minutes portion of format MM:SS
+  // Math.floor removes fractional minute remainder
+  const minutes = Math.floor(totalMinutes);
+
+  // Calculate seconds portion of format MM:SS and set to 2 digits
+  // (totalMinutes - minutes) results in a fractional minute
+  // Multiply by 60 to get secs
+  const seconds = ((totalMinutes - minutes) * 60).toPrecision(2);
+
+  return `${minutes}:${seconds}`;
+}
