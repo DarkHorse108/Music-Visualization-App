79cc1140f7045fa86d3b17c14271f598e2f0d600
John Sy <13071774+DarkHorse108@users.noreply.github.com>
2020-10-23 05:09:20 -1000

    main.js is the renamed event_listener.js module

---
+++ Music_Visualization_Application/static/js/main.js
@@ -0,0 +1,32 @@
+import { requestTrack } from "./soundcloud_request_module.js";
+import {
+  displayErrorMessage,
+  clearErrorMessage,
+} from "./error_message_module.js";
+const id = null;
+
+// This variable will hold the string value entered by the user in the submission form.
+let user_url = null;
+
+// We capture the form HTML element associated with the submission form for the SoundCloud URL by the user
+const form_element = document.getElementById("user_form_element");
+
+// We add an eventlistener when the form has been submitted (i.e. when the submit button is pressed). We prevent refreshing of the page
+// and then take the string value the user entered in the form and store it in the variable user_url. We then clear the form of any text and print the captured value.
+form_element.addEventListener("submit", function (event) {
+  event.preventDefault();
+  user_url = document.getElementById("user_input_url").value;
+
+  form_element.reset();
+  clearErrorMessage();
+
+  const results = requestTrack(user_url, id)
+    .then((promise_variable) => {
+      console.log(promise_variable);
+      //Update portion using promise_variable object
+    })
+    .catch((error_variable) => {
+      displayErrorMessage();
+      console.log(error_variable.message);
+    });
+});
