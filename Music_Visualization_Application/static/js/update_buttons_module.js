2350d3700f435b7b3a03ff6964531e11ad282e11
John Sy <13071774+DarkHorse108@users.noreply.github.com>
2020-10-24 12:46:46 -1000

    Created module and exported functions for updating play/pause icons.

---
+++ Music_Visualization_Application/static/js/update_buttons_module.js
@@ -0,0 +1,7 @@
+export function displayPauseButton() {
+  $("#play_pause_icon").attr("class", "fas fa-pause");
+}
+
+export function displayPlayButton() {
+  $("#play_pause_icon").attr("class", "fas fa-play");
+}
