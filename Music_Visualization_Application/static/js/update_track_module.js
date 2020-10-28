34a972b84b83e76429b6c71f2881e07faafce491
John Sy <13071774+DarkHorse108@users.noreply.github.com>
2020-10-27 14:04:40 -1000

    Added formatTime, display_time_elapsed and display_progressbar_elapsed functions to the update track/music module

--- Music_Visualization_Application/static/js/update_track_module.js
+++ Music_Visualization_Application/static/js/update_track_module.js
@@ -13,8 +13,35 @@ function loadTrackArtist(SoundCloud_track) {
   $("#track_artist").html(SoundCloud_track.artist);
 }
 
+// Loads the total duration of the song, in minutes:seconds format i.e. (1:15)
 function loadTrackDuration(SoundCloud_track) {
   $("#track_end_time").html(SoundCloud_track.duration);
+}
+
+// Converts an argument representing units of time in seconds, into a format reflecting minutes:seconds as a string and returns it.
+// This snippet was taken from the Howler.js documentation's music player example found here: https://github.com/goldfire/howler.js/blob/master/examples/player/player.js
+function formatTime(total_seconds_elapsed) {
+  var formatted_minutes = Math.floor(total_seconds_elapsed / 60) || 0;
+  var formatted_seconds = total_seconds_elapsed - formatted_minutes * 60 || 0;
+
+  return (
+    formatted_minutes +
+    ":" +
+    (formatted_seconds < 10 ? "0" : "") +
+    formatted_seconds
+  );
+}
+
+// Takes an argument representing units of time in seconds, converts it into minutes:seconds format and places that value as the value of the paragraph element with id "track_current_time".
+export function display_time_elapsed(seconds_elapsed) {
+  let formatted_time = formatTime(Math.round(seconds_elapsed));
+  $("#track_current_time").html(formatted_time);
+}
+
+// Takes a numerical (integer or float) value as an argument, representing a percentage. This value is placed as the value of the range input element with id "track_seeker" which shows the
+// progress of the played track as a progress/seeker bar.
+export function display_progressbar_elapsed(percentage_elapsed) {
+  $("#track_seeker").val(percentage_elapsed);
 }
 
 // Wrapper function that updates all relevant information regarding the audio, art, track title, and track artist using the SOundCloud_track object
