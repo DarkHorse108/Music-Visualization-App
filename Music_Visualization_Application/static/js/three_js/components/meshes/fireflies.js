f898650afb2d9136f0025e627df43dc0e5bbde9a
John Sy <13071774+DarkHorse108@users.noreply.github.com>
2020-11-28 02:12:14 -1000

    Updated firefly default color, and intensity-based behavior for opacity, scaling, and y axis jump potential.

--- Music_Visualization_Application/static/js/three_js/components/meshes/fireflies.js
+++ Music_Visualization_Application/static/js/three_js/components/meshes/fireflies.js
@@ -4,20 +4,19 @@ import {
   SphereBufferGeometry,
 } from "../../three.module.js";
 import { getRandomFloat, getRandomInt } from "../buildWorld.js";
-export { createFireFly };
 
 function createFireFly(fireFlyNumber) {
   // Allowable x, y, and z range for each firefly
   const xRange = { min: -45, max: 48 };
-  const yRange = { min: 8, max: 35 };
+  const yRange = { min: 4, max: 30 };
   const zRange = { min: -52, max: 45 };
 
   // default speed of the firefly as it travels in the positive x and z axes direction
   const fireFlySpeed = 0.02;
 
   // default color in hex of a firefly
-  const defaultColor = 0xcbf3f7;
-
+  const defaultColor = 0xffff7a;
+  // 0xcbf3f7
   //Size of each firefly and number of segments in its geometry, which influences its shape. We choose irregular spherical shapes so that each firefly looks distinct.
   const fireFlyRadius = 0.2;
   const fireFlySegments = getRandomInt(1, 5);
@@ -25,6 +24,8 @@ function createFireFly(fireFlyNumber) {
   // Material, geometry, and mesh
   const material = new MeshBasicMaterial({
     color: defaultColor,
+    transparent: true,
+    opacity: 0.7,
   });
   const geometry = new SphereBufferGeometry(
     fireFlyRadius,
@@ -80,32 +81,40 @@ function createFireFly(fireFlyNumber) {
       // If this particular firefly has some frequency value returned from the song that is non zero, we change its color and scale
       if (intensity > 0) {
         // Increase its scale to make it more noticeable
-        mesh.scale.set(1.8, 1.8, 1.8);
+        mesh.scale.set(2, 2, 2);
 
         // If this firefly is one of the fireflies representing an index within the firs 0-30 (inclusive) indices of the frequency array
         if (mesh.fireFlyId <= 30) {
           // Give it a primarily yellow color whose intensity is dependent on how large the frequency value it represents in the freqArray
           mesh.material.color.setRGB(intensity + 0.5, 0.79, 0);
-          // Give its y axis maxGain to be 15. The first 0-30 indices are generally high frequency notes and are the most common. We want these to be very noticeable so not only are they yellow in color,
-          // They have the highest potential for a y-axis jump/increase so they can be easily seen above mid to low frequencies in the song.
-          mesh.maxGain = 15;
+          // Give its y axis maxGain to be 10. The first 0-30 indices are generally high frequency notes and are the most common. We want these to be very noticeable so they are yellow in color,
+          mesh.maxGain = 10;
 
           // If this firefly is one of the fireflies representing an index within the second grouping of 31-60 (inclusive) indices of the frequency array
         } else if (31 <= mesh.fireFlyId && mesh.fireFlyId <= 60) {
           // Give it a primarily purple color whose intensity is dependent on how large the frequency value it represents in the freqArray
           mesh.material.color.setRGB(intensity, 0.41, 0.59);
-          // Give its y axis maxGain to be 10. The 31-60 indices are generally mid frequency notes and are the second most common. We want these to be noticeable but also be distinct from the high frequencies, so not only are they purple  in color,
-          // They have a lower potential for a y-axis jump/increase so they can be easily seen above low frequencies in the song. Generally we should see the high frequency/red fireflies at the top, followed by the mid frequencies in blue below it.
-          mesh.maxGain = 10;
+          // Give its y axis maxGain to be 15. The 31-60 indices are generally mid frequency notes and are the second most common. We want these to be noticeable but also be distinct from the high frequencies, so not only are they purple in color,
+          // They have a lower potential for a y-axis jump/increase so they can be easily seen near the low-range frequencies in the song that they often accompany. Generally we should see the high frequency/red fireflies easily based on color, followed by the mid frequencies in blue above it.
+          mesh.maxGain = 15;
+          mesh.scale.set(
+            intensity * 2 + 2,
+            intensity * 2 + 2,
+            intensity * 2 + 2
+          );
 
           // If this firefly is one of the fireflies representing an index within the third grouping of 61-99 (inclusive) indices of the frequency array
         } else if (61 <= mesh.fireFlyId && mesh.fireFlyId <= 99) {
           // Give it a primarily darker blue color whose intensity is dependent on how large the frequency value it represents in the freqArray
-          mesh.material.color.setRGB(0.33, 0.58, intensity + 0.5);
-          // Give its y axis maxGain to be 5. The 61-99 indices are generally low frequency notes and are not as commonly given values in songs. We want these to be noticeable but also be distinct from the other frequencies, so they are more blue.
-          // They have a lower potential for a y-axis jump/increase so they can be easily seen distinctly below the other frequencies.
-          mesh.maxGain = 5;
+          mesh.material.color.setRGB(0.33, 0.58, intensity + 0.4);
+          // Give its y axis maxGain to be 25. The 61-99 indices are generally mid-low frequency notes and accompany the mid level frequencies in percussions in songs, and are utilized extensively in popular music.
+          // We want these to be noticeable but also be distinct from the other frequencies, so they are more blue.
+          // They have a very high potential for a y-axis jump/increase so they can be easily seen distinctly above the other frequencies.
+          mesh.maxGain = 25;
         }
+
+        // Set the opacity to correspond to the frequency intensity, with all fireflies with data having a minimum of 0.6 opacity so they are visible, but become much more so the higher the frequency intensity they receive.
+        mesh.material.opacity = intensity + 0.6;
 
         // Since the if statements above divide the fireflies by color and y Axis jump potential, they actual Y axis jump value is calculated here depending on which category the firefly belongs to, high, mid, or low frequencies.
         mesh.position.y = randomY + intensity * mesh.maxGain;
@@ -121,3 +130,5 @@ function createFireFly(fireFlyNumber) {
   // Return the firefly mesh with all of the necessary properties and update method.
   return mesh;
 }
+
+export { createFireFly };
