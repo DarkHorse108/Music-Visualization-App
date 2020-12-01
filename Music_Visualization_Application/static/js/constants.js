9265675e77e301c00cc94be15486858635f11acd
penhs@oregonstate.edu <46614782+penhs@users.noreply.github.com>
2020-11-30 20:33:31 -0800

    Update to the default song

--- Music_Visualization_Application/static/js/constants.js
+++ Music_Visualization_Application/static/js/constants.js
@@ -5,9 +5,10 @@ const INPUT_FORM = document.getElementById("user_form_element");
 const PLAY_BUTTON = document.getElementById("play_pause_button");
 const VOLUME_UP_BUTTON = document.getElementById("volume_up_button");
 const VOLUME_DOWN_BUTTON = document.getElementById("volume_down_button");
+
 // Initialize the default track
-const DEFAULT_TRACK =
-  "https://ia802807.us.archive.org/23/items/cd_mozart_wolfgang-amadeus-mozart-richard-goode/disc1/07.%20Wolfgang%20Amadeus%20Mozart%20-%20Rondo%20in%20A%20minor%2C%20K.%20511_sample.mp3";
+const DEFAULT_TRACK = "./static/mp3/furacao.mp3";
+
 // The quantity of fast fourier transform samples of the track's frequency to be sampled per call to the Analyzer Node.
 const FREQUENCY_SAMPLESIZE = 256;
 
